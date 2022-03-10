798643e0ac48870e853469f1aaa61adf6e0c9b4f
was2309 <vasilijem92@gmail.com>
2022-03-10 22:57:13 +0100

    Dodata klasa Magacin sa praznom implementacijom interfejsa MagacinInterfejs

---
+++ Magacin/src/magacin/Magacin.java
@@ -0,0 +1,25 @@
+package magacin;
+
+import magacinInterfejs.MagacinInterfejs;
+
+public class Magacin implements MagacinInterfejs {
+
+	@Override
+	public void dodajArtikal(Artikal artikal) {
+		
+
+	}
+
+	@Override
+	public void izbaciArtikal(Artikal artikal) {
+		
+
+	}
+
+	@Override
+	public Artikal vratiArtikal(Long sifra) {
+		
+		return null;
+	}
+
+}
