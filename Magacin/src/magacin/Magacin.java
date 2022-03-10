40283a492630a8137f6ac70244f51379f0202c6e
was2309 <vasilijem92@gmail.com>
2022-03-10 23:09:30 +0100

    Uradjena implementacija metoda iz MagacinInterfejs-a u klasi Magacin

--- Magacin/src/magacin/Magacin.java
+++ Magacin/src/magacin/Magacin.java
@@ -1,24 +1,50 @@
 package magacin;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import magacinInterfejs.MagacinInterfejs;
 
 public class Magacin implements MagacinInterfejs {
+	
+	private List<Artikal> artikli = new LinkedList<>();
 
 	@Override
 	public void dodajArtikal(Artikal artikal) {
-		
-
+		for(Artikal a:artikli) {
+			if(a.getSifra().equals(artikal.getSifra())) {
+				a.setKolicina(a.getKolicina()+1);
+				break;
+			}
+			else {
+				artikli.add(artikal);
+				break;
+			}
+		}
 	}
 
 	@Override
 	public void izbaciArtikal(Artikal artikal) {
-		
+		for(Artikal a:artikli) {
+			if(a.getSifra().equals(artikal.getSifra()) && a.getKolicina()>0 ) {
+				a.setKolicina(a.getKolicina()-1);
+				break;
+			}
+			else {
+				artikli.remove(artikal);
+				break;
+			}
+		}
 
 	}
 
 	@Override
 	public Artikal vratiArtikal(Long sifra) {
-		
+		for(Artikal a:artikli) {
+			if(a.getSifra().equals(sifra)) {
+				return a;
+			}
+		}
 		return null;
 	}
 
